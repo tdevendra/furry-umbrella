dad2474b15fae9e5370e0f9d4c54257adcc43254
Devendra Tapdia <devendratapdia@gmail.com>
2016-12-26 15:09:22 +0000

    Adding more examples

---
+++ ml/wordcount.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from py4j.java_gateway import java_import
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
+        exit(-1)
+
+    sc = SparkContext(appName="PythonStreamingKafkaWordCount")
+    ssc = StreamingContext(sc, 10)
+    java_import(ssc._sc._gateway.jvm, "org.apache.spark.sql.*")
+
+    zkQuorum, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
